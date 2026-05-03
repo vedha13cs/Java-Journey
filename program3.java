c1f6318c8654c4de59089edbb093a944b43a28e8
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-03 23:37:20 +0530

    Add MultipleLines class with main method

---
+++ program3.java
@@ -0,0 +1,9 @@
+public class MultipleLines {
+    public static void main(String[] args) {
+
+        System.out.println("Java");
+        System.out.println("Programming");
+        System.out.println("Journey");
+
+    }
+}
