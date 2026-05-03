05e6c8a36984f07058ab435a7631a1bd38444ecb
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-03 23:36:11 +0530

    Add PrintDetails class to display user information

---
+++ program2.java
@@ -0,0 +1,9 @@
+public class PrintDetails {
+    public static void main(String[] args) {
+
+        System.out.println("Name: Surabhi");
+        System.out.println("Course: Computer Science");
+        System.out.println("City: Bangalore");
+
+    }
+}
