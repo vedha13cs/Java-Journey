7bb776e7a9ff3bfcc58d6d23e378170aa8e52d82
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-04 22:04:37 +0530

    Add RectangleArea class to calculate area

---
+++ program5.java
@@ -0,0 +1,12 @@
+public class RectangleArea {
+    public static void main(String[] args) {
+
+        int length = 10;
+        int width = 5;
+
+        int area = length * width;
+
+        System.out.println("Area = " + area);
+
+    }
+}
