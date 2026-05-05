990675ef8e79a08374fe4e88a085364a2fd4ce16
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-05 20:50:08 +0530

    Add LargestNumber class to find larger of two integers

---
+++ program8.java
@@ -0,0 +1,14 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+
+        int a = 25;
+        int b = 18;
+
+        if (a > b) {
+            System.out.println(a + " is larger");
+        } else {
+            System.out.println(b + " is larger");
+        }
+
+    }
+}
