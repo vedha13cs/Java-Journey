c9ff67d0371fdfe2f12236237418d0aac2ecf84b
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-06 22:53:26 +0530

    Create VariablesDemo class with variable examples
    Initial implementation of VariablesDemo class to demonstrate variable declaration and output.

---
+++ program11.java
@@ -0,0 +1,13 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+        int age = 20;
+        float marks = 85.5f;
+        char grade = 'A';
+
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+
+    }
+}
