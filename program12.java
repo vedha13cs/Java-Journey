568e9288490ce76b698f0a7b616523311b48ddf4
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-06 22:51:13 +0530

    Add DataTypesDemo class with main method

---
+++ program12.java
@@ -0,0 +1,15 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = 20.5;
+        char c = 'S';
+        boolean isJavaFun = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(isJavaFun);
+
+    }
+}
