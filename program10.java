c5e537c5352cb0f2bdb1ef4a7a0d547ab1e1a128
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-06 22:52:19 +0530

    Add ASCIIValue class to print ASCII of a character

---
+++ program10.java
@@ -0,0 +1,11 @@
+public class ASCIIValue {
+    public static void main(String[] args) {
+
+        char ch = 'A';
+
+        int ascii = ch;
+
+        System.out.println("ASCII Value = " + ascii);
+
+    }
+}
