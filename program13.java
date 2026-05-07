4771e262722c653f2d450ad98cb5493a40213263
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-07 20:41:50 +0530

    Add TypeCasting class for double to int conversion

---
+++ program13.java
@@ -0,0 +1,11 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        double num = 10.75;
+        int result = (int) num;
+
+        System.out.println("Double: " + num);
+        System.out.println("Integer: " + result);
+
+    }
+}
