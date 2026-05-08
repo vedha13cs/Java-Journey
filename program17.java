6156be4e8b75dde9114e98dc8bdad27e73881373
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-08 16:48:02 +0530

    Implement swap of two integers without a temp variable

---
+++ program17.java
@@ -0,0 +1,14 @@
+public class SwapWithoutTemp {
+    public static void main(String[] args) {
+
+        int a = 5, b = 10;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
