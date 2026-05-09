fd0eec4a9edd285b7ea559b7732e6a951f94196c
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-09 15:43:19 +0530

    Add IncrementDecrement class with main method

---
+++ program21.java
@@ -0,0 +1,13 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        a++;
+        System.out.println("After Increment: " + a);
+
+        a--;
+        System.out.println("After Decrement: " + a);
+
+    }
+}
