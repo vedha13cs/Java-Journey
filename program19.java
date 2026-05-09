5c5aba2f1acdd533d699e111db7640be1380085c
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-09 15:41:58 +0530

    Add RelationalOperators class to demonstrate comparisons

---
+++ program19.java
@@ -0,0 +1,13 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 20;
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+    }
+}
