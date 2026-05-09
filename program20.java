3ebb77f4779248a59386073edd42788cb8ada3c0
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-09 15:42:37 +0530

    Add LogicalOperators class with basic operations

---
+++ program20.java
@@ -0,0 +1,11 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        System.out.println(age > 18 && age < 25);
+        System.out.println(age > 25 || age < 30);
+        System.out.println(!(age > 18));
+
+    }
+}
