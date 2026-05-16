ebb376cec2d5c541f3868069b45e91541a983388
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-16 18:51:38 +0530

    Add LargestOfThree class to find the largest number

---
+++ program25.java
@@ -0,0 +1,15 @@
+public class LargestOfThree {
+    public static void main(String[] args) {
+
+        int a = 10, b = 25, c = 15;
+
+        if (a > b && a > c) {
+            System.out.println(a + " is largest");
+        } else if (b > c) {
+            System.out.println(b + " is largest");
+        } else {
+            System.out.println(c + " is largest");
+        }
+
+    }
+}
