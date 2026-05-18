e6ee4c273e459cbbf885031cb752cb9d26eec647
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-18 23:25:03 +0530

    Add If-Else statement to check even or odd number

---
+++ program29.java
@@ -0,0 +1,13 @@
+public class IfElseStatement {
+    public static void main(String[] args) {
+
+        int number = 5;
+
+        if(number % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+
+    }
+}
