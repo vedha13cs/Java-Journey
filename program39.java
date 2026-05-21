0a27573d68fc6814918bf0cf54ab208c05829c19
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-21 21:59:14 +0530

    Add multiplication table for number 5

---
+++ program39.java
@@ -0,0 +1,11 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        int number = 5;
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+
+    }
+}
