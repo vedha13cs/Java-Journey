42d702fec46addd9fa2ccd579d1838287874b76f
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-22 23:46:26 +0530

    Implement factorial calculation in program40.java

---
+++ program40.java
@@ -0,0 +1,14 @@
+public class Factorial {
+    public static void main(String[] args) {
+
+        int number = 5;
+        int factorial = 1;
+
+        for(int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial = " + factorial);
+
+    }
+}
