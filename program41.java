f083dece8884018926058ee984512a1098d6d45f
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-22 23:45:21 +0530

    Add Fibonacci series implementation in program41.java

---
+++ program41.java
@@ -0,0 +1,19 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+
+        int a = 0, b = 1;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        for(int i = 1; i <= 8; i++) {
+
+            int c = a + b;
+            System.out.println(c);
+
+            a = b;
+            b = c;
+        }
+
+    }
+}
