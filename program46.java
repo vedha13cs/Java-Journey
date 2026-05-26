f26dc3ce313575151b01ecf5df17660e8588fef2
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-26 23:53:10 +0530

    Add StringComparison class for string equality check

---
+++ program46.java
@@ -0,0 +1,13 @@
+public class StringComparison {
+    public static void main(String[] args) {
+
+        String s1 = "Java";
+        String s2 = "Java";
+
+        if(s1.equals(s2)) {
+            System.out.println("Strings are Equal");
+        } else {
+            System.out.println("Strings are Not Equal");
+        }
+    }
+}
