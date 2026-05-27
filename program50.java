354c7e0a28df9b267864a3663ed000a315faba17
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-27 22:50:22 +0530

    Add CountWords class to count words in a sentence

---
+++ program50.java
@@ -0,0 +1,10 @@
+public class CountWords {
+    public static void main(String[] args) {
+
+        String sentence = "Java is easy to learn";
+
+        String[] words = sentence.split(" ");
+
+        System.out.println("Words = " + words.length);
+    }
+}
