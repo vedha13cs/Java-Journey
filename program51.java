6364aeb0b95c6b458a8ac755e160acb55fc5dde7
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-27 22:46:57 +0530

    Add StringConcatenation class to print full name

---
+++ program51.java
@@ -0,0 +1,11 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+
+        String firstName = "Surabhi";
+        String lastName = "Vedha";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+    }
+}
