b77c88abae028b2c7fd7cf6073b8cd4b21aff334
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-28 22:29:42 +0530

    Add AdditionMethod class with sum calculation

---
+++ program53.java
@@ -0,0 +1,16 @@
+public class AdditionMethod {
+
+    static void add() {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Sum = " + (a + b));
+    }
+
+    public static void main(String[] args) {
+
+        add();
+
+    }
+}
