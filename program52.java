7a7ed8e4de7acc28863a1b31ca2d1539323545f2
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-28 22:28:52 +0530

    Add MethodExample class with display method

---
+++ program52.java
@@ -0,0 +1,12 @@
+public class MethodExample {
+
+    static void display() {
+        System.out.println("Welcome to Java Methods");
+    }
+
+    public static void main(String[] args) {
+
+        display();
+
+    }
+}
