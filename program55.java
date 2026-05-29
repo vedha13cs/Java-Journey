4dafa2ef039c82f3a53486afe468e8fb82ee84c1
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-29 22:25:02 +0530

    Add greet method to print a greeting message

---
+++ program55.java
@@ -0,0 +1,12 @@
+public class ParameterMethod {
+
+    static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+
+        greet("Vedhavathi");
+
+    }
+}
