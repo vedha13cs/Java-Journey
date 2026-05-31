2ed484d4c4e4029eaf3da636a7c8cd0185bd945c
Vedhavathi Y A <vedha9782@gmail.com>
2026-05-31 23:52:08 +0530

    Implement method overloading for addition

---
+++ program58.java
@@ -0,0 +1,17 @@
+public class MethodOverloading {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(add(10, 20));
+        System.out.println(add(5.5, 4.5));
+
+    }
+}
