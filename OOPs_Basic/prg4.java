b1e860bcc496ab6f401f5c314d086ec3361f68c5
Vedhavathi Y A <vedha9782@gmail.com>
2026-06-14 23:53:27 +0530

    Add Student class with name and age attributes

---
+++ OOPs_Basic/prg4.java
@@ -0,0 +1,13 @@
+public class Student {
+
+    String name = "Vedha";
+    int age = 21;
+
+    public static void main(String[] args) {
+
+        Student s = new Student();
+
+        System.out.println("Name: " + s.name);
+        System.out.println("Age: " + s.age);
+    }
+}
