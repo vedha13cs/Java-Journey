bf56f4e5efb61c4bccca55ecc75c095e054e85ef
Vedhavathi Y A <vedha9782@gmail.com>
2026-06-14 23:54:02 +0530

    Add Car class with brand attribute and main method

---
+++ OOPs_Basic/prg5.java
@@ -0,0 +1,11 @@
+public class Car {
+
+    String brand = "Toyota";
+
+    public static void main(String[] args) {
+
+        Car c = new Car();
+
+        System.out.println("Brand: " + c.brand);
+    }
+}
