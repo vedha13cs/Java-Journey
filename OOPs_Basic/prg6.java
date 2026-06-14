348d0a0480dc8c64b60603f9c24912daa0ca18ce
Vedhavathi Y A <vedha9782@gmail.com>
2026-06-14 23:58:52 +0530

    Add Employee class with name and salary attributes

---
+++ OOPs_Basic/prg6.java
@@ -0,0 +1,13 @@
+public class Employee {
+
+    String name = "John";
+    double salary = 50000;
+
+    public static void main(String[] args) {
+
+        Employee e = new Employee();
+
+        System.out.println("Name: " + e.name);
+        System.out.println("Salary: " + e.salary);
+    }
+}
