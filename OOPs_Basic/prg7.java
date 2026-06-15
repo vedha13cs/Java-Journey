40c7c0820e5a75156768061ce60a5fdd48306842
Vedhavathi Y A <vedha9782@gmail.com>
2026-06-15 23:45:57 +0530

    Add ConstructorDemo class with a simple constructor

---
+++ OOPs_Basic/prg7.java
@@ -0,0 +1,12 @@
+public class ConstructorDemo {
+
+    ConstructorDemo() {
+        System.out.println("Constructor Called");
+    }
+
+    public static void main(String[] args) {
+
+        ConstructorDemo obj = new ConstructorDemo();
+
+    }
+}
