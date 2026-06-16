a3c85d6c574952befe54f0543dbc176f3b8a95cb
Vedhavathi Y A <vedha9782@gmail.com>
2026-06-16 23:36:33 +0530

    Add DefaultConstructor class with a default constructor

---
+++ Constructor_java/prg1.java
@@ -0,0 +1,12 @@
+public class DefaultConstructor {
+
+    DefaultConstructor() {
+        System.out.println("Object Created Successfully");
+    }
+
+    public static void main(String[] args) {
+
+        DefaultConstructor obj = new DefaultConstructor();
+
+    }
+}
