3c2edd8d8329e94185f1277e573098c731d70785
Vedhavathi Y A <vedha9782@gmail.com>
2026-06-21 00:07:34 +0530

    Add StaticBlock class with static initializer

---
+++ keyword/prg5.java
@@ -0,0 +1,14 @@
+public class StaticBlock {
+
+    static {
+
+        System.out.println("Static Block Executed");
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Main Method");
+
+    }
+}
