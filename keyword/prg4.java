2491891ea14d478f342da417606e77bfd6685c90
Vedhavathi Y A <vedha9782@gmail.com>
2026-06-21 00:08:18 +0530

    Add StaticMethod class with greeting method

---
+++ keyword/prg4.java
@@ -0,0 +1,14 @@
+public class StaticMethod {
+
+    static void greet() {
+
+        System.out.println("Welcome to Java");
+
+    }
+
+    public static void main(String[] args) {
+
+        greet();
+
+    }
+}
