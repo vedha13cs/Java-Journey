ad7f8227aebfaa98f24e53cb1adad78d130eacb6
Vedhavathi Y A <vedha9782@gmail.com>
2026-07-05 23:34:42 +0530

    Add file creation functionality in prg1.java

---
+++ FileHandling/prg1.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File file = new File("StudentData.txt");
+
+            if (file.createNewFile()) {
+                System.out.println("File Created Successfully");
+            } else {
+                System.out.println("File Already Exists");
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
