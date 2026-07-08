0f40602fd6393ab9c3bff1964b4b8a5a0489185d
Vedhavathi Y A <vedha9782@gmail.com>
2026-07-08 23:55:57 +0530

    Add LinkedListDemo to demonstrate LinkedList usage

---
+++ JavaCollectionFramework/prg2.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> cities = new LinkedList<>();
+
+        cities.add("Bangalore");
+        cities.add("Mysore");
+        cities.add("Mangalore");
+
+        System.out.println(cities);
+    }
+}
