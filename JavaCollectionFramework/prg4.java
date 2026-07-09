ac1bdac03e916973c35ff4222f534d2c20404308
Vedhavathi Y A <vedha9782@gmail.com>
2026-07-09 21:33:10 +0530

    Add TreeSetDemo class to demonstrate TreeSet usage

---
+++ JavaCollectionFramework/prg4.java
@@ -0,0 +1,16 @@
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+    public static void main(String[] args) {
+
+        TreeSet<Integer> marks = new TreeSet<>();
+
+        marks.add(85);
+        marks.add(70);
+        marks.add(95);
+        marks.add(80);
+
+        System.out.println(marks);
+    }
+}
