ba53675fad2a8278f65e5dcdfbdc9b36e97b4716
Vedhavathi Y A <vedha9782@gmail.com>
2026-07-22 21:14:17 +0530

    Add MaxOfThree.java to find maximum of three numbers

---
+++ Intermediate_java/MaxOfThree.java
@@ -0,0 +1,11 @@
+public class MaximumNumber {
+
+    public static void main(String[] args) {
+
+        int a = 45, b = 72, c = 18;
+
+        int max = Math.max(a, Math.max(b, c));
+
+        System.out.println("Maximum = " + max);
+    }
+}
