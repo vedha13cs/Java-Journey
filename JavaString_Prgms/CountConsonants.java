e737cd34681c3deccf0234e715695ded7adca665
Vedhavathi Y A <vedha9782@gmail.com>
2026-07-29 18:44:57 +0530

    Add CountConsonants class to count consonants in a string

---
+++ JavaString_Prgms/CountConsonants.java
@@ -0,0 +1,25 @@
+public class CountConsonants {
+
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+        int count = 0;
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z' &&
+                ch != 'a' && ch != 'e' &&
+                ch != 'i' && ch != 'o' &&
+                ch != 'u') {
+
+                count++;
+            }
+        }
+
+        System.out.println("Consonants: " + count);
+    }
+}
