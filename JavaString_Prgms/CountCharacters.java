c0c6e52286ca652eb12c8e74ebefdd5c22d5ee46
Vedhavathi Y A <vedha9782@gmail.com>
2026-07-30 22:34:00 +0530

    Add CountCharacters class to count string length

---
+++ JavaString_Prgms/CountCharacters.java
@@ -0,0 +1,9 @@
+public class CountCharacters {
+
+    public static void main(String[] args) {
+
+        String str = "Programming";
+
+        System.out.println("Characters: " + str.length());
+    }
+}
