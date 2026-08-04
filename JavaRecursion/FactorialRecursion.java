3b972f5a09f0602c7c896c2b7b71edda26f702bd
Vedhavathi Y A <vedha9782@gmail.com>
2026-08-04 22:40:34 +0530

    Implement recursive factorial function

---
+++ JavaRecursion/FactorialRecursion.java
@@ -0,0 +1,12 @@
+public class FactorialRecursion {
+
+    static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial = " + factorial(5));
+    }
+}
