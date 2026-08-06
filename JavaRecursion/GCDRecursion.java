dbb69f16ceacb434b8edab1692c3ced586d7f196
Vedhavathi Y A <vedha9782@gmail.com>
2026-08-06 23:51:26 +0530

    Add GCD calculation using recursion in GCDRecursion.java

---
+++ JavaRecursion/GCDRecursion.java
@@ -0,0 +1,14 @@
+public class GCDRecursion {
+
+    static int gcd(int a, int b) {
+
+        if (b == 0)
+            return a;
+
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("GCD = " + gcd(24, 18));
+    }
+}
